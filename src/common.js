c00d70fcd65fd69e6a4f5423de35fde4abca9155
A1rPun <thombakker@gmail.com>
2019-06-12 02:55:58 +0200

    Simple negative & exponent numbers

--- src/common.js
+++ src/common.js
@@ -1,17 +1,24 @@
-export function createNumeral(numerals) {
+export function createNumeral(numerals, negative = '-', dot = '.') {
   return {
     parse(str) {
       return +str
         .split('')
-        .map(x => numerals.indexOf(x))
+        .map(x => (x === negative || x === dot ? x : numerals.indexOf(x)))
         .join('');
     },
     to(n) {
       return n
         .toString()
         .split('')
-        .map(x => numerals[x])
+        .map(x => (x === negative || x === dot ? x : numerals[x]))
         .join('');
     },
   };
+}
+
+export function toBase(n, base = 10) {
+  return n.toString(base).toUpperCase();
+}
+export function parseBase(str, base = 10) {
+  return parseInt(str, base);
 }
