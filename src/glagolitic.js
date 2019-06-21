8a7dc208ac591da4653a01a2098e149502e7fc03
A1rPun <thombakker@gmail.com>
2019-06-21 03:47:14 +0200

    Make glagolitic alphabetic numeral

--- src/glagolitic.js
+++ src/glagolitic.js
@@ -1,17 +1,11 @@
-const glagolitic = [
+import { createAlphabeticNumeral } from './common';
+
+export const {
+  parse: parseGlagolitic,
+  to: toGlagolitic,
+} = createAlphabeticNumeral([
   ['', 'Ⰰ', 'Ⰱ', 'Ⰲ', 'Ⰳ', 'Ⰴ', 'Ⰵ', 'Ⰶ', 'Ⰷ', 'Ⰸ'], // ones
   ['', 'Ⰺ', 'Ⰻ', 'Ⰼ', 'Ⰽ', 'Ⰾ', 'Ⰿ', 'Ⱀ', 'Ⱁ', 'Ⱂ'], // tens Ⰹ = 10
   ['', 'Ⱃ', 'Ⱄ', 'Ⱅ', 'Ⱆ', 'Ⱇ', 'Ⱈ', 'Ⱉ', 'Ⱋ', 'Ⱌ'], // hundreds
   ['', 'Ⱍ', 'Ⱎ', 'Ⱏ', 'Ⱑ', 'Ⱓ', 'Ⱗ', '', '', ''], // thousands Ⱐ = 3000
-];
-
-export function parseGlagolitic(str) {
-  throw Error('Not implemented');
-}
-export function toGlagolitic(n) {
-  return n
-    .toString()
-    .split('')
-    .reverse()
-    .reduce((acc, x, i) => (i < glagolitic.length ? glagolitic[i][x] : '') + acc, '');
-}
+]);
